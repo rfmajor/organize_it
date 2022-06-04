45078284aff34d01688b73d780831860d67535de
mrospond <marekrospond@gmail.com>
2022-06-04 18:20:56 +0200

    model package added

---
+++ android/app/src/main/java/pl/major/filip/organize_it/model/topic/Topic.java
@@ -0,0 +1,9 @@
+package pl.major.filip.organize_it.model.topic;
+
+import java.util.HashSet;
+
+public class Topic {
+    private String name;
+    private String subject;
+    private HashSet<Attachment> attachments;
+}
